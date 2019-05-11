f24627e7dc394ff0e0fb2b6dd47ec37677c7f919
Magda Bula <magdalena.bula@gmail.com>
2019-05-11 20:39:32 +0200

    Column done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,13 @@ class App extends React.Component {
 		return (
 			<main className={styles.component}>
 				<h1 className={styles.title}>My first React app</h1>
-				<h2 className={styles.subtitle}>Hello world</h2>
-				<List />
+				<h2 className={styles.subtitle}>A simple to-do app, with lists, columns and cards</h2>
+				<List
+					title={['Things to do in space ', <sup key="1">soon!</sup>]}
+					image={'https://i.postimg.cc/7YLXTY4x/space.png'}
+				>
+					<p>Intresting things I want to ckeck out!</p>
+				</List>
 			</main>
 		);
 	}
