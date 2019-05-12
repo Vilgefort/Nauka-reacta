23ed19175a217a6f9dca67732318b3d6e673cd2d
Magda Bula <magdalena.bula@gmail.com>
2019-05-12 08:15:55 +0200

    Cards done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
+import { pageContents, listData } from '../../data/dataStore';
 
 class App extends React.Component {
 	render() {
 		return (
 			<main className={styles.component}>
-				<h1 className={styles.title}>My first React app</h1>
-				<h2 className={styles.subtitle}>A simple to-do app, with lists, columns and cards</h2>
-				<List
-					title={['Things to do in space ', <sup key="1">soon!</sup>]}
-					image={'https://i.postimg.cc/7YLXTY4x/space.png'}
-				>
-					<p>Intresting things I want to ckeck out!</p>
-				</List>
+				<h1 className={styles.title}>{pageContents.title}</h1>
+				<h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+				<List {...listData} />
 			</main>
 		);
 	}
---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Icon = props => <i className={`fas fa-${props.name}`} />;
+
+Icon.propTypes = {
+	name: PropTypes.node,
+};
+
+export default Icon;
