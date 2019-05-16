d5bb3b8b5122e2ea897c6e16349923033a56a5ea
Magda Bula <magdalena.bula@gmail.com>
2019-05-16 19:55:52 +0200

    Adding cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,18 +2,22 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-//import Creator from '../Creator/Creator.js';
-//import { settings } from '../../data/dataStore';
+import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
+import { settings } from '../../data/dataStore';
 
 class Column extends React.Component {
   static propTypes = {
     title: PropTypes.node,
     cards: PropTypes.array,
     icon: PropTypes.node,
+    addCard: PropTypes.func,
+  };
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
   };
   render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
@@ -23,13 +27,12 @@ class Column extends React.Component {
           </span>
         </h3>
         <div className={styles.cards}>{cards.map(cardData => (<Card key={cardData.id} {...cardData} />))}
-        </div>{/*
+        </div>
         <div className={styles.creator}>
           <Creator
             text={settings.cardCreatorText}
-            action={title => this.addCard(title)}
-          />
-          </div>*/}
+            action={addCard}
+          /></div>
       </section>
     );
   }
--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -1,8 +1,10 @@
 import {connect} from 'react-redux';
 import Column from './Column.js';
+import {getCardForColumn, createAction_addCard } from '../../redux/cardsRedux.js';
 
-export const getCardsForColumns = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
+const mapStateToProps = (state, props) => ({cards: getCardForColumn(state, props.id)});
 
-const maoStateToProps = (state, props) => ({cards: getCardsForColumns(state, props.id)});
-
-export default connect(maoStateToProps)(Column);
+const mapDispatchToProps = (dispatch, props) => ({addCard: title => dispatch(createAction_addCard({columnId: props.id,title,
+})),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(Column);
