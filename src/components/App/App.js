e9f575c10c466158336a84a04a334b55c8ddf1e1
Magda Bula <magdalena.bula@gmail.com>
2019-05-23 09:06:52 +0200

    Heroku

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Home from '../Home/HomeContainer.js';
 import Info from '../Info/Info';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout.js';
 import FAQ from '../FAQ/FAQ.js';
-import {AnimatedSwitch} from 'react-router-transition';
+import { AnimatedSwitch } from 'react-router-transition';
 import styles from './App.scss';
 import List from '../List/ListContainer.js';
 import Search from '../Search/SearchContainer.js';
